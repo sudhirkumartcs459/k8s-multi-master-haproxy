56dda16eede7da49dc08265926b6290ba4e4b76d
Sudhir Kumar <59279871+sudhirkumartcs459@users.noreply.github.com>
2025-06-07 00:10:14 +0530

    disable-swap.sh

--- setup/01-disable-swap.sh
+++ setup/01-disable-swap.sh
@@ -1,1 +1,5 @@
+To run the Kubernetes Cluster we should make sure that swap shlould be disabled on the Master , HAProxy Nodes as well as Worker Node.
 
+#!/bin/bash
+swapoff -a
+sed -i '/ swap / s/^/#/' /etc/fstab
