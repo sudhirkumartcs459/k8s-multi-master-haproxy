1dec3a6d5654d46c6e169d591987188154828212
Sudhir Kumar <59279871+sudhirkumartcs459@users.noreply.github.com>
2025-06-07 00:24:40 +0530

    install-k8s.sh

--- setup/03-install-k8s.sh
+++ setup/03-install-k8s.sh
@@ -1,1 +1,8 @@
+Now we have to install the kubelet , kubeadm and kubcectl.
 
+#sudo apt-get update
+#sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+#curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+#echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#sudo apt-get update
+#sudo apt-get install -y kubelet kubeadm kubectl
